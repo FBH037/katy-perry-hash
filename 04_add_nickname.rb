9ed37dd85da5a1d52a3e8f66252a854180973c79
Finnegan Hewitt <fbhewitt@gmail.com>
2015-02-09 09:57:46 -0700

    Submitting Exercise

--- 04_add_nickname.rb
+++ 04_add_nickname.rb
@@ -1,6 +1,6 @@
 require_relative "person"
 
-KATY_PERRY[nickname: "KP"]
+KATY_PERRY[:nickname] = "KP"
 
 puts KATY_PERRY[:nickname]
 # Add a nickname to the KATY_PERRY hash and print it out. Don't edit the person.rb file!
--- 05_print_address.rb
+++ 05_print_address.rb
@@ -1,3 +1,5 @@
 require_relative "person"
 
+puts "  #{KATY_PERRY[:address][:street]}, #{KATY_PERRY[:address][:street]}, #{KATY_PERRY[:address][:state]}, #{KATY_PERRY[:address][:zip_code]}" 
+
 # Print Katy's address. It should read "123 Hollywood Boulevard, Los Angeles, CA, 90210"
